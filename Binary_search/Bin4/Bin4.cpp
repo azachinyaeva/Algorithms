b11e36354416de860e91a120c5096933ee59b254
Anna <annahasagun@yahoo.com>
2023-04-07 21:23:29 +0300

    bin4

---
+++ Binary_search/Bin4/Bin4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <iomanip>
+
+long double search(long double c) {
+	long double left = 0;
+	long double right = c;
+	while ((right - left) > 1e-10)
+	{
+		long double middle = (left + right) / 2;
+		if (middle * middle + sqrt(middle) < c)
+		{
+			left = middle;
+		}
+		else 
+		{
+			right = middle;
+		}
+	}
+	return right;
+}
+
+int main() {
+	long double c;
+	std::cin >> c;
+	std::cout << std::fixed << std::setprecision(10) << search(c);
+}
