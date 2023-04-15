fc5ad22153c82567d8240486bfd63504562fcba4
Anna <annahasagun@yahoo.com>
2023-04-16 00:47:58 +0300

    fix

--- Binary_search/Bin_6/Bin_6.cpp
+++ Binary_search/Bin_6/Bin_6.cpp
@@ -1,28 +1,9 @@
 ﻿#include <iostream>
 
-void quick_sort(int* arr, int size)
-{
-	int i = 0;
-	int j = size - 1;
-	int mid = arr[(i + j) / 2];
-
-	do {
-
-		while (arr[i] < mid) { i++; }
-		while (arr[j] > mid) { j--; }
-		if (i <= j)
-		{
-			std::swap(arr[i++], arr[j--]);
-		}
-	} while (i <= j);
-	if (j > 0) { quick_sort(arr, j + 1); }
-	if (i < size) { quick_sort(&arr[i], size - i); }
-}
-
 int search(int* arr, int size, int k) {
 	int left = 0;
 	int right = 10000001;
-	while (left+1 < right)
+	while ((right - left) > 1)
 	{
 		int middle = (left + right) / 2;
 		int count = 0;
@@ -50,7 +31,6 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		std::cin >> arr[i];
 	}
-	quick_sort(arr, n);
 	std::cout << search(arr, n, k);
 	delete[] arr;
 }
