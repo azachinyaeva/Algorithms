32223267ff62f2bd85a9e9319038e63fd1cf7b21
Anna <annahasagun@yahoo.com>
2023-04-16 11:04:34 +0300

    Bin7

---
+++ Binary_search/Bin_7/Bin_7.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	long long n, x, y;
+	std::cin >> n >> x >> y;
+	--n;
+	long long left = 0, right = n * std::max(x, y), middle;
+	while (left < right) {
+		middle = (left + right) / 2;
+		if ((middle / x) + (middle / y) < n) {
+			left = middle+1;
+		}
+		else {
+			right = middle;
+		}
+	}
+	std::cout << right + std::min(x, y);
+
+}
---
+++ Binary_search/Bin_7/readme.md
@@ -0,0 +1,1 @@
+![readme6](https://user-images.githubusercontent.com/120329586/231557707-7460671d-9399-4e3a-8f9c-b0d2af3dfd34.jpg)
